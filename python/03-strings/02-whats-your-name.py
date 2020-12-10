04bb19ff367e461472282b1f1454964a5e3f6786
atalaydev <omer@atalay.dev>
2020-12-10 22:15:18 +0300

    add solution for whats-your-name

---
+++ python/03-strings/02-whats-your-name.py
@@ -0,0 +1,8 @@
+def print_full_name(fn, ln):
+    print(f"Hello {fn} {ln}! You just delved into python.")
+
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
