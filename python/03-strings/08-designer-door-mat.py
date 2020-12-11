31a88cfa8f21d2813857d4b3b91f570bd64a9e29
atalaydev <omer@atalay.dev>
2020-12-11 22:52:52 +0300

    add solution for designer-door-mat

---
+++ python/03-strings/08-designer-door-mat.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    for i in range(1, n, 2):
+        print((i * '.|.').center(m, '-'))
+
+    print('WELCOME'.center(m, '-'))
+
+    for i in reversed(range(1, n, 2)):
+        print((i * '.|.').center(m, '-'))
