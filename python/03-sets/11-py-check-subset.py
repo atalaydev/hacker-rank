90f9682d4ef8076f496bbb2c112a5e4692fee357
atalaydev <omer@atalay.dev>
2021-01-23 23:56:28 +0300

    add solution for py-check-subset

---
+++ python/03-sets/11-py-check-subset.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    results = []
+    t = int(input())
+    for s in range(t):
+        _ = input()
+        a = set(map(int, input().split()))
+        _ = input()
+        b = set(map(int, input().split()))
+
+        (
+            results.append('True') if a.issubset(b) 
+            else results.append('False')
+        )
+
+    print('\n'.join(results))
